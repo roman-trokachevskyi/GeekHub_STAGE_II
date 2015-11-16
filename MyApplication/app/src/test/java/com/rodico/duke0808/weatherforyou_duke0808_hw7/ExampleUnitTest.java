624dc24519ec5015d4e8e9ec495bdad7dad79c7b
roman-trokachevskyi <dukenukem3d0808@gmail.com>
2015-11-16 20:08:42 +0200

    First Commit

---
+++ MyApplication/app/src/test/java/com/rodico/duke0808/weatherforyou_duke0808_hw7/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.rodico.duke0808.weatherforyou_duke0808_hw7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
